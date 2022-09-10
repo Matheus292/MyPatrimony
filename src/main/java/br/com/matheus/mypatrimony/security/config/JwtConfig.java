cddc58c80ceca0b429e351c101302415a886341c
Matheus292 <matheusrbarbosa97@gmail.com>
2022-09-09 23:54:20 -0300

    Liberando o cors

--- src/main/java/br/com/matheus/mypatrimony/security/config/JwtConfig.java
+++ src/main/java/br/com/matheus/mypatrimony/security/config/JwtConfig.java
@@ -42,7 +42,8 @@ public class JwtConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.csrf().disable().authorizeRequests()
+        http.cors().and()
+                .csrf().disable().authorizeRequests()
                 .antMatchers(HttpMethod.POST, "/login").permitAll()
                 .antMatchers(HttpMethod.POST, "/api/signUp").permitAll()
                 .antMatchers(HttpMethod.POST, "/api/forgotPassword").permitAll()
